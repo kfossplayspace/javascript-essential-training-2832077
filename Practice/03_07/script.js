06a6a8d28fceb4b1c66a263eccb1756ddc2382be
kfossplayspace <fosskm@msn.com>
2025-04-02 19:48:49 +0000

    commit changes

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,15 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+/**
+ * Create a Car
+ * /
+ Const car = {
+    name: "Automobile",
+    passengers: 6,
+    color: red,
+    model: "Ford",
+    Features:{
+       heatedSeats: true,
+       navigation: false},
+    }
